0f355bdffcc878665983274b79cda5a8d4432128
Leo <leo0223perez@outlook.com>
2023-05-23 09:20:14 -0400

    Added cors and route login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+app.use(cors()); 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("API esta en funcionamiento");
-  });
+  res.send("API esta en funcionamiento");
+});
+
+app.use("/login", require("./Routes/RoutesLogin"));
 
 module.exports = app;
