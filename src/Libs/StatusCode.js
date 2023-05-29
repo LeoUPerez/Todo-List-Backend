c7dfa2d26773d2ef36feabae81599f2dfbfafc1f
Leo <leo0223perez@outlook.com>
2023-05-29 09:19:24 -0400

    adding new https responder

---
+++ src/Libs/StatusCode.js
@@ -0,0 +1,11 @@
+ const StatusCode = {
+    Ok: 200,
+    Created: 201,
+    Accepted: 202,
+    NotAcceptable: 406,
+    NotFound: 404,
+    LengthRequired: 411
+}
+
+module.exports = StatusCode;
+
